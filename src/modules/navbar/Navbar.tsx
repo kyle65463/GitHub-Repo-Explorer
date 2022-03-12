e428c745114babaea7163163e4301eff2aad174b
kyle65463 <kyle65463@gmail.com>
2022-03-12 16:24:59 +0800

    Add simple NavBar

---
+++ src/modules/navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link";
+import React, { useCallback } from "react";
+
+export default function Navbar() {
+	const router = useRouter();
+
+	const onLogoClick = useCallback(() => {
+		router.push("/");
+	}, []);
+
+	return (
+		<nav className='navbar bg-base-100 shadow-sm'>
+			<div className='flex-1'>
+				<a className='btn btn-ghost normal-case text-xl' onClick={onLogoClick}>
+					GitHub Repo Viewer
+				</a>
+			</div>
+			<div className='flex-none'>
+				<ul className='menu flex-row p-0'>
+					<li>
+						<Link href='/about'> About </Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import Navbar from "modules/navbar/Navbar";
 import type { AppProps } from "next/app";
 import "../global.scss";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div className="bg-base-200 min-h-screen">
+			<Navbar />
 			<Component {...pageProps} />
 		</div>
 	);
