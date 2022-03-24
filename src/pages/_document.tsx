2a23f9541832c9cf4ba43af0776ef3793d1822e4
kyle65463 <kyle65463@gmail.com>
2022-03-24 11:19:14 +0800

    Update document title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,18 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
 				<Head>
+					<meta property='og:title' content='GitHub Viewer' key='title' />
 					<link href='https://fonts.googleapis.com/css2?family=Inter&display=optional' rel='stylesheet' />
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' />
-					<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=League+Spartan:wght@400;500;600&family=Roboto+Slab:wght@400;500;600;700&family=Roboto:wght@400;500;700&family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet"></link>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Ubuntu:wght@400;500;600;700&display=swap'
+						rel='stylesheet'
+					/>
 				</Head>
 				<body>
 					<Main />
