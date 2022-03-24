b302cc48c228ba126b1fb09cf08e1c38fb0a0a46
kyle65463 <kyle65463@gmail.com>
2022-03-24 11:24:59 +0800

    Fix document title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<meta property='og:title' content='GitHub Viewer' key='title' />
+					<title>GitHub Viewer</title>
 					<link href='https://fonts.googleapis.com/css2?family=Inter&display=optional' rel='stylesheet' />
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' />
